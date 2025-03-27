ecd510f9b174ac2b4f6f44695ebb82f8b4b1b1a0
ColdEgg98 <leytriss@gmail.com>
2025-03-27 17:42:35 +0900

    [Feat] FSM 관련 작성중
    IState → 상태 인터페이스
    StateMachine → 상태를 관리
    PlayerAnimationData → 플레이어 상태가 정의됨
    Player → 상태를 가짐

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class Player : Character
 {
+    [field: Header("Animations")]
+    [field: SerializeField] public PlayerAnimationData animationData { get; private set; }
+
+    public Animator animator { get; private set; }
+    public PlayerController input { get; private set; }
+    public Rigidbody controller { get; private set; }
+
+    private void Awake()
+    {
+        animationData.Initialize();
+        animator = GetComponent<Animator>();
+        input = GetComponent<PlayerController>();
+        controller = GetComponent<Rigidbody>();
+    }
+
 
 }
