2be00210018ff5bb08fb8f17803e6383480ec6f1
ColdEgg98 <leytriss@gmail.com>
2025-04-02 14:36:50 +0900

    [Fix] TakeDamage 관련

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Player : Character
@@ -8,12 +7,15 @@ public class Player : Character
     [field: SerializeField] public PlayerAnimationData AnimationData { get; private set; }
     [field: SerializeField] public PlayerSO Data { get; private set; }
 
-    private PlayerStateMachine stateMachine;
+    protected internal PlayerStateMachine stateMachine;
+
+    public Action detectTakeDamage;
 
     public Animator Animator { get; private set; }
     public PlayerController Input { get; private set; }
     public Rigidbody RigidBody { get; private set; }
 
+    public float RunSpeed { get; private set; } = 7f;
 
     private void Awake()
     {
@@ -39,5 +41,15 @@ public class Player : Character
     private void FixedUpdate()
     {
         stateMachine.PhysicsUpdate();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Character collisionOBJ = collision.gameObject.GetComponent<Character>();
+            float damage = collisionOBJ.stat.Attack;
+            TakeDamage(damage);
+        }
     }
 }
